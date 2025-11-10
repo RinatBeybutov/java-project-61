5351ff4e63c5964b4365f82d6a7f973aeed319c1
Ринат <ignat934@gmail.com>
2025-11-10 19:03:13 +0300

    перенос проекта

---
+++ code/app/build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    id("java")
+    application
+    //id("checkstyle")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+application { mainClass.set("org.example.Main") }
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.getByName("run", JavaExec::class) {
+    standardInput = System.`in`
+}
+
+/*
+checkstyle {
+    toolVersion="9.2.1"
+}*/
